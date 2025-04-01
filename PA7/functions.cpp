94b645f00b9aedb9c64a5336f7de2f63b4425810
Aabhwan Adhikary <aabhwan.adhikary@gmail.com>
2025-03-31 19:06:04 -0700

    temporary pa 7 commit

---
+++ PA7/functions.cpp
@@ -0,0 +1,25 @@
+/*
+* Programmer: Aabhwan Adhikary
+* Class: CptS 122, Spring 2025; Lab Section 9
+* Assignment: PA 7
+* Created: 3/31/2025
+* Description: This file contains the function definitions of the main program.
+*/
+
+#include "Header.hpp"
+
+void importCourseList()
+{
+	ifstream courseListFile;
+	courseListFile.open("classList.csv", ios::in);
+
+	string line;
+	getline(courseListFile, line); // reads the first line of the file
+
+	while (getline(courseListFile, line)) {
+		// iterates until there is no line in courseListFile to read
+
+	}
+
+	courseListFile.close();
+}
